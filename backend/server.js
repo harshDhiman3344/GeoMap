666423053043bf66b2b0f920c274f7abeac06903
HarshDhiman <142768740+harshDhiman3344@users.noreply.github.com>
2026-06-04 11:46:05 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ const app = express()
 
 
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://geo-map-three.vercel.app', 'http://localhost:5173']
+}));
 app.use(express.json());
 
 app.use('/api/journals',require('./routes/journals'));
